2693e25ac9b3327c93c1338a17f6507381324a39
Orbit <75210244+strOrbit@users.noreply.github.com>
2022-03-27 00:55:00 +0300

    Update Encryptor.java

--- Encryptor.java
+++ Encryptor.java
@@ -1,9 +1,5 @@
 public class Encryptor {
     public static int encrypt(String password) {
-        if (password.contains(" "))
-            throw new IllegalArgumentException("Password cannot contains space!");
-        else if (password.length() <= 8)
-            throw new IllegalArgumentException("Password must be contains 8 or above characters!");
         int encryptedValue = 0;
         for (char c : password.toCharArray()) {
             encryptedValue += c * 65536;
